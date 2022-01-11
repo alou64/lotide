d54f33f19efc26e1b82d60682389a3160d0679a8
alou64 <81368883+alou64@users.noreply.github.com>
2022-01-11 08:01:58 -0800

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,13 +2,15 @@ const assertArraysEqual = (arr1, arr2) => {
   const tmsg = `✅✅✅ Assertation Passed: ${arr1} === ${arr2}`;
   const fmsg = `🛑🛑🛑 Assertation Failed: ${arr1} !== ${arr2}`;
 
-  if (arr1.length !== arr2.length) console.log(fmsg);
+  if (arr1.length !== arr2.length) {
+    console.log(fmsg);
+    return;
+  }
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) console.log(fmsg);
     return;
   }
+  
   return console.log(tmsg);
 };
-
-assertArraysEqual([1,2,3], [1,2,3,4]);
