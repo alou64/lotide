c016ce9e5a0d0933dcadec7b00875aeb1f30c82b
alou <01008@protonmail.com>
2022-01-11 11:22:59 -0800

    implemented letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = str => {
+  let ans = {};
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toUpperCase() !== str[i].toLowerCase()) {
+      let ch = str[i].toLowerCase();
+      if (!ans[ch]) {
+        ans[ch] = [i];
+      } else {
+        ans[ch].push(i);
+      }
+    }
+  }
+  return ans;
+};
