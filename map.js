37c3c7a8ad0bf038bccba1e93b94a9983d29dfaf
alou <01008@protonmail.com>
2022-01-12 16:13:28 -0800

    updated assertArraysEqual, implemented map function

--- map.js
+++ map.js
@@ -15,3 +15,17 @@ const assertArraysEqual = (arr1, arr2) => {
   }
   console.log(`🛑🛑🛑 Assertation Failed: ${arr1} !== ${arr2}`);
 };
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+let arr = [1, 2, 3];
+assertArraysEqual(map(arr, num => num + 1), [2, 3, 4]);
+assertArraysEqual(map(arr, num => num * 3), [3, 6, 9]);
+assertArraysEqual(map(arr, num => num.toString()), ['1', '2', '3']);
